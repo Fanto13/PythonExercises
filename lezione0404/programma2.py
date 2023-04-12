7fc5e1a59eb70500607239e9e35c4d335840d872
CFI-MSI-21\17346_21 <fantiniifts@gmail.com>
2023-04-12 18:00:02 +0200

    update

--- lezione0404/programma2.py
+++ lezione0404/programma2.py
@@ -1,65 +1,60 @@
-stream = open("lezione0412/laboratorio.txt") #apre il file in lettura
-
-l = []
-for item in stream:
-    i = item.find(":")
-    spazio = item[0:i]
-    item = item[i+2:]
+def stampa_ripetuta(parola, numero=1, has_cornice=False): #se c'è un argomento opzionale deve essere messo per ultimo
+    print(parola * numero)
     
-    media = 0
-    for j in range(4):
-        i = item.find(" ")
-        tempo = item[0:i]
-        item = item[i+1:]
+stampa_ripetuta("mirco ")#è possibile utilizzare il polimorfismo, e quindi passare i il numero di argomenti
+stampa_ripetuta("mirco ", has_cornice=True, numero=5)#se non specifico l'ordine devo rispettare l'ordine posizionale degli argomenti
 
-        media += float(spazio) / float(tempo)
+import math  #importo tutte le funzioni di math
+print(math.ceil(7.8568)) #arrotonda per eccesso, mi devo ricordare di mettere math prima della funzione se non ho importato tutto
+print(max(1, 5, 9, 85, 68, 47, 548, 25, 35)) #mi ritorna il massimo tra i numeri passati come argomenti
+print(min(1, 5, 9, 85, 68, 47, 548, 25, 35)) #mi ritorna il minimo tra i numeri passati come argomenti
+def coppia():
+    return "pippo", "pluto", "paperino", "minnie" #posso ritornare più valori, in questo caso due stringhe
 
-    # calcolo velocità
-    media /= 4
+a,b, *__= coppia() #posso assegnare più variabili alla volta, in questo caso a e b, con __* ignoro il resto dei valori, salti tutti i valori tranne il primo ritornato
+print(b)
 
-    print(media)
-stream.close()
+def visibilita():
+    a="casa"
     
-with open("lezione0412/laboratorio.txt") as stream:
-    L = []
-    for item in stream:
-        L = item.strip().split(":") #strip toglie gli spazi iniziali e finali, split divide la stringa in una lista di stringhe, usando come separatore il carattere passato come parametro (in questo caso : )
-        spazio = L[0] #prendo l'elemento 0 della lista
-        tempi = L[1]  #prendo l'elemento 1 della lista
+print(a)#non posso stampare a perchè è una variabile locale, non è visibile fuori dalla funzione
 
-        numeri = tempi.strip().split() #strip toglie gli spazi iniziali e finali, split divide la stringa in una lista di stringhe, usando come separatore lo spazio
+txt=input("ciao, inserisci il tuo nome: ")
+print("quello che hai inserito è:", txt)
+
+if txt=="mario rossi":#si possono anche omettere le parentesi tonde
+   print("venuto")
+elif txt=="gino paoli":
+    print("malvenuto")
+else:
+    print("malvenuto")
+
+# while txt != "luca verdi":#la struttura del while invece di usare la selezione effettua l'iterazione, i while con attenzione perchè possono creare un loop infinito
+#     txt = input("utente non riconosciuto, inserisci il tuo nome: ")
+    
+if not ((5==5) and (4 < 9)):
+    print("sei bravo in mate")
+
+for lettera in "parola":
+    print(lettera)#stampa ogni carattere uno dopo l'altro
+    
+a="caramella alla fragola"
+#print(len(trova))
+#stampare tutti gli indici in cui è presente la lettera a 
+
+for i in range(len(a)): 
+    if a[i]=="a":
+        print(i)
         
-        media = 0
-        for tempo in numeri:
-            media += float(spazio) / float(tempo) #calcolo la velocità media
+print("\n")        
+#in modo analogo si può fare utilzzando il find utilizzando il while
 
-        res = [] 
-        media /= 4
-        res.append(media)
-        print("res: ", res)
-        with open ("res.txt" , "w") as file:
-            #mi scorre tutti gli elementi
-            res = [str(x)for x in res] #trasforma tutti gli elementi della lista in stringhe e li mette in una nuova lista che viene assegnata a res
-            stringa = " - ".join(res) #qui concatenano tutti gli elementi della lista con il carattere passato come parametro
-            print(stringa)
-            
 
-#stream.close()
+while a.find("a") != -1:
+    i=a.find("a")
+    print(a)
+    
+    a=a[i+1:]#a=a[i+1:] mi permette di continuare a cercare la lettera a a partire dall'indice successivo a quello trovato
 
-#come aprire un file in python in maniera migliore
-file = open("nuovo.txt", "w") #apre il file in scrittura, se esiste lo sovrascrive
-file.write("ciao\n nuova linea")
-file.writelines("ciao\n n\nuova linea")
 
-lista = ["ciao", "nuova linea"]
-def writelines(file, lista):
-    for linea in lista:
-        file.write(linea)
-        file.write("\n")
-
-file.close()
-        
-#in python c'è l'ereditarietà, il polimorfismo ma non l'incapsulamento
-
-#i dizionari sono simili alle liste ma hanno chiavi e valori si usano le graffe
-{'nome': 'mario', 'cognome': 'rossi'}
+    
